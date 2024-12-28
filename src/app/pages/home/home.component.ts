5af3e7afa5418d04a7742ebddeb202e22d84feff
Marcos Segantine <msegantine8@gmail.com>
2024-12-27 23:11:16 -0300

    feat: creating basic struct from project

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+import { InputComponent } from '../../components/input/input.component';
+
+@Component({
+  selector: 'app-home',
+  imports: [InputComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+}
